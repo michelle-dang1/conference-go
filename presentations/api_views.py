396fe50b883c306ad7c87db13e16bea8afbd862c
Michelle Dang <michelledangny@gmail.com>
2023-03-17 13:24:52 -0400

    Completed D2: DIY JSON Library

--- presentations/api_views.py
+++ presentations/api_views.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 
 from .models import Presentation
+from common.json import ModelEncoder
 
 
 def api_list_presentations(request, conference_id):
@@ -36,44 +37,22 @@ def api_list_presentations(request, conference_id):
     return JsonResponse({"presentations": presentations})
 
 
+class PresentationDetailEncoder(ModelEncoder):
+    model = Presentation
+    properties = [
+        "presenter_name",
+        "company_name",
+        "presenter_email",
+        "title",
+        "synopsis",
+        "created",
+    ]
+
+
 def api_show_presentation(request, id):
-    """
-    Returns the details for the Presentation model specified
-    by the id parameter.
-
-    This should return a dictionary with the presenter's name,
-    their company name, the presenter's email, the title of
-    the presentation, the synopsis of the presentation, when
-    the presentation record was created, its status name, and
-    a dictionary that has the conference name and its URL
-
-    {
-        "presenter_name": the name of the presenter,
-        "company_name": the name of the presenter's company,
-        "presenter_email": the email address of the presenter,
-        "title": the title of the presentation,
-        "synopsis": the synopsis for the presentation,
-        "created": the date/time when the record was created,
-        "status": the name of the status for the presentation,
-        "conference": {
-            "name": the name of the conference,
-            "href": the URL to the conference,
-        }
-    }
-    """
     presentation = Presentation.objects.get(id=id)
     return JsonResponse(
-        {
-            "presenter_name": presentation.presenter_name,
-            "company_name": presentation.company_name,
-            "presenter_email": presentation.presenter_email,
-            "title": presentation.title,
-            "synopsis": presentation.synopsis,
-            "created": presentation.created,
-            "status": presentation.status.name,
-            "conference": {
-                "name": presentation.conference.name,
-                "href": presentation.conference.get_api_url(),
-            },
-        }
+        presentation,
+        encoder=PresentationDetailEncoder,
+        safe=False,
     )
